4cc4384ea251d9fb661cb050932be878ce1b6836
betulozer <32954011+betulozer@users.noreply.github.com>
2019-09-17 16:11:18 +0300

    Update soru2.go

--- soru2.go
+++ soru2.go
@@ -7,16 +7,24 @@ type Queue struct {
 }
 
 // Append adds the given element at the end of the queue
-func Append() {}
+func Append(q *Queue) {
+	Queue = append(Queue, e)
+}
 
 // AppendLeft adds the given element at the head of the queue
-func AppendLeft() {}
+func AppendLeft(q *Queue) {
+	Queue = append([]int{"i"},Queue...)
+}
 
 // Pop removes the latest element in the queue
-func Pop() {}
+func Pop(q *Queue) {
+	e, Queue := Queue[len(Queue)-1], Queue[:len(Queue)-1]
+}
 
 // Pop removes the first element in the queue
-func PopLeft() {}
+func PopLeft(qu *Queue) {
+	e, Queue := Queue[0], Queue[:len(Queue)-1]
+}
 
 func main() {
 	q := &Queue{}
